9a4b0e595f9b1062c14fc4e84468d2b614726d4a
gr33ndata <tarekamr@gmail.com>
2021-12-18 14:17:01 +0100

    ipython

---
+++ ipython/.ipython/profile_default/startup/10-dbconn.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('fivethirtyeight')
+
+sys.path.append('/Users/<user>/Workshop')
